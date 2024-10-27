0cfd6878962df142660850f47f5e6fb05f945a38
Roman Miroshnychenko <roman.miroshnychenko@comeet.co>
2024-10-27 14:59:16 +0200

    Add a module docstring

--- weather.open-meteo.lite/libs/common/kodi_service.py
+++ weather.open-meteo.lite/libs/common/kodi_service.py
@@ -12,7 +12,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
+"""Classes and functions to interact with Kodi API"""
 import datetime
 import hashlib
 import json
