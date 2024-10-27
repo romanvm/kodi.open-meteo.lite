8be90fd380c8c540fcb6a4d9e71becbbc9946c0b
Roman Miroshnychenko <roman.miroshnychenko@comeet.co>
2024-10-27 15:12:27 +0200

    Add a module docstring

--- weather.open-meteo.lite/libs/converter_service.py
+++ weather.open-meteo.lite/libs/converter_service.py
@@ -12,6 +12,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""Functions that convert various weather parameters"""
 
 import enum
 
